e83e1ebee91bfe08c3d0b2424626ad4b1965dd64
jonathas-silva <silva.jonathas@outlook.com>
2022-05-15 18:22:26 -0300

    Props

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -11,15 +11,36 @@ function Listing() {
     //forma não recomendada - apenas para teste de requisição 
 
 
-    //const [pageNumber, setPageNumber] = useState();
+    const [pageNumber, setPageNumber] = useState(0);
+
+    const [page, setPage] = useState<MoviePage>({
+        content: [],
+        last: true,
+        totalPages: 0,
+        totalElements: 0,
+        size: 12,
+        number: 0,
+        first: true,
+        numberOfElements: 0,
+        empty: true
+    })
+
     useEffect(() => {
-        axios.get(`${BASE_URL}/movies?size=12&page=0`).then(
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`).then(
             response => {
                 const data = response.data as MoviePage;
-                console.log(data)
+                setPage(data);
             }
-        ); 
-    }, []);
+        );
+    }, [pageNumber]);
+
+    const movie = {
+        id: 1,
+        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
+        title: "The Witcher",
+        count: 2,
+        score: 4.5
+    };
 
     return (
 
@@ -28,26 +49,11 @@ function Listing() {
 
             <div className="container">
                 <div className="row">
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        {/* essa classe indica que a partir de telas sm o componente vai ocupar 6 colunas*/}
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        {/* essa classe indica que a partir de telas sm o componente vai ocupar 6 colunas*/}
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        {/* essa classe indica que a partir de telas sm o componente vai ocupar 6 colunas*/}
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        {/* essa classe indica que a partir de telas sm o componente vai ocupar 6 colunas*/}
-                        <MovieCard />
-                    </div>
-                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
-                        {/* essa classe indica que a partir de telas sm o componente vai ocupar 6 colunas*/}
-                        <MovieCard />
-                    </div>
+                    {page.content.map(movie => (
+                        <div key={movie.id} className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                            <MovieCard movie={movie} />
+                        </div>
+                    ))}
                 </div>
             </div>
 
